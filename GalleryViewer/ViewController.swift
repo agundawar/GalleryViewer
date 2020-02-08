be12627715d4c54c3e1a42fab8ed8440911726df
agundawar <akhil.g@browserstack.com>
2020-02-08 16:05:02 +0530

    Initial Commit

---
+++ GalleryViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GalleryViewer
+//
+//  Created by Akhil Gundawar on 08/02/20.
+//  Copyright © 2020 Akhil Gundawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
